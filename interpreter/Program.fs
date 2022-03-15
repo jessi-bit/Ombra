465c09c3d2354d13b6cbdf95f6dea212878faacd
jessica <jessica.vecchia@gmail.com>
2022-03-16 00:02:51 +0100

    attempt

--- interpreter/Program.fs
+++ interpreter/Program.fs
@@ -16,12 +16,36 @@ type OToken =
     | OLiteralString of s : string
     | OIdentifier of s : string
 
+type Expression =
+    | OToken
+    | List of Expression
+
+
 let categorize token =
     match parseFloat token with
         | Some f -> OLiteralFloat f
         | None -> match token with
                       | s when s.StartsWith "\"" && s.EndsWith "\"" -> OLiteralString (s.Substring(1, (s.Length) - 1))
                       | s -> OIdentifier s
+
+
+// let parenthesize tokens result =
+//     match List.head tokens with
+//     | OIdentifier "(" -> let innerResult = parenthesize List.tail tokens 
+//                          parenthesize list.Tail tokens result @ innerResult
+//     | OIdentifier ")" -> result
+//     | _ ->
+
+let parenthesize tokens result =
+    match List.head tokens with
+    | OIdentifier "(" -> let innerResult = parenthesize List.tail tokens 
+                         parenthesize list.Tail tokens result @ innerResult
+    | OIdentifier ")" -> result
+    | _ ->
+
+
+
+
 
 
 // TODO add custom types to tokenised and parenthesized?
@@ -32,5 +56,14 @@ let rec parenthesize tokenised parenthesized =
         |
 *)
 
-
 let result = tokenize "(+ 1 (+ 1 40))"
+(*
+[
+    OIdentifier "+"; OLiteralFloat 1;
+    [
+        OIdentifier "+"; OLiteralFloat 1; OLiteraFloat 40
+    ]
+
+]
+*)
+
