1bfb39c6778a62f7e5e618d769651910a434f800
Alberto Zaccagni <montecristoh@gmail.com>
2022-05-19 23:31:08 +0200

    wip

--- interpreter/ast.fs
+++ interpreter/ast.fs
@@ -11,12 +11,14 @@ type value =
     | S of string
 
 type env = E of Map<var, value>
+let emptyEnv = E Map.empty
 
 // JessiBit's idea: in Lisp everything is a list, so code it accordingly
 // TODO refactor exp in exps
 type exp =
     | Value of value
     | Var of var
+    // TODO remove me?
     | Symbol of string
     | Function of (exp list -> env -> exp)
     | List of exp list
@@ -24,7 +26,6 @@ type exp =
 // ---------------------------------------------
 // Environment
 
-let globalEnv = E (Map.add (V "x") (K 2) Map.empty)
 exception NotFound of var
 
 let print (E env) =
@@ -47,56 +48,50 @@ let intersect (E outer) (E inner) =
 // If we get to this stage it means we already type checked
 // the expression so it's safe to make assumptions
 
-let rec eval2 exps env =
+
+let rec eval exps env =
     match exps with
         | Value (K k) -> Value (K k)
         | Var x -> Value (find x env)
         | List (exp::exps) ->
             match exp with
                 | Function funx -> funx exps env
-                | _ -> List (exp :: List.map (fun exp -> eval2 exp env) exps)
+                | _ -> List (exp :: List.map (fun exp -> eval exp env) exps)
         | _ ->
             printf "%A\n" exps
             failwith "error 2"
 
-// qui ho aggiunto il type constructor cosi non serve specificare il tipo,
-// pero' boh
-let emptyEnv = E Map.empty
-
 // ---------------------------------------------
-// Example ASTs
-
-// Questi li ho spostati qui perche' poi l'idea e' di metterli in un
-// modulo a parte dove faremo i test, e li serviranno quindi anche da doc
-
-// Forse qui ho esagerato, cosa ne pensi?
-// E' che mi piaceva la separazione tra value e exp... pero' porta
-// a questo risultato, cioe' al wrap di K 41 dentro un Value
+// Native functions
 
 let rec plus exp env =
     match exp with
         | [] -> Value (K 0)
-        | (Value (K k))::tail ->
-            let result = plus tail env
-            match result with
-                | Value (K k') -> Value (K (k + k'))
-                | _ -> failwith "CAZZO"
         // TODO Find variable in env
+        | (Value (K k))::tail ->
+            let (Value (K k')) = plus tail env
+            Value (K (k + k'))
         | _ ->
             printf "%A\n" exp
             failwith "cannot call sum on this"
 
+let lambda body env =
+    (fun args ->
+     let innerEnv = intersect env args
+     eval body innerEnv)
+
+
 let sumAst = List [(Function plus); (Value (K 1)); (Value (K 41))]
+let res = eval sumAst (emptyEnv)
+
 let listAst = List [(Value (K 1)); (Value (K 41))]
+let res2 = eval listAst (emptyEnv)
 
-let res = eval2 listAst (emptyEnv)
+let onlyFunctionList = Function plus
 
-//let sumWithMax = FunCall (O "+", [Value (K 41); Value (K 1);
-//                         FunCall (O "max", [Value (K 1); Value (K 2)])]
-
-
-//    a     b       n
-//                    x
-
-let a y = 
-    (fun x -> x + y) 2
+(*
+((lambda (funx)
+ (funx 41 1)
+) (lambda (a b) (+ a b)))
+*)
+List [(Function lambda) (List [(Function plus); (Var a); (Var b)])]
