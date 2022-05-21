42aa40e0550cad5874877efe92655787e90fb44d
jessica <jessica.vecchia@gmail.com>
2022-05-21 18:28:40 +0200

    added more tests on our cases

--- interpreter.tests/UnitTest2.fs
+++ interpreter.tests/UnitTest2.fs
@@ -17,8 +17,17 @@ let TestConfig () =
 let TestSum () =
     let exp = List [Symbol "+"; Value (K 1); Value (K 41)] 
     let env = emptyEnv
-    match (eval env exp) with
+    match (eval exp env) with
         | Value (K k) -> Assert.AreEqual (42, k)
+        | _ -> Assert.Fail()
+    Assert.Pass()
+
+[<Test>]
+let TestSumWithVars () =
+    let exp = List [Symbol "+"; Var (V "x"); Var (V "y"); Value (K 5)] 
+    let env = (E (Map.add (V "x") (K 2) (Map.add (V "y") (K 2) Map.empty)))
+    match (eval exp env) with
+        | Value (K k) -> Assert.AreEqual (4 k)
         | _ -> Assert.Fail()
     Assert.Pass()
 
@@ -26,7 +35,29 @@ let TestSum () =
 let TestLambdaBase1 () =
     let invokedLambda = List [List [Symbol "lambda"; List []; Value (K 41)]]
     let astUsingLambda = List [Symbol "+"; (Value (K 1)); invokedLambda]
-    match (eval env exp) with
+    let env = emptyEnv
+    match (eval astUsingLambda env) with
         | Value (K k) -> Assert.AreEqual (42, k)
+        | _ -> Assert.Fail()
+    Assert.Pass()
+
+[<Test>]
+let TestLambdaBase2 () =
+    let invokedLambda2 = List [List [Symbol "lambda"; List [Var (V "x")]; Var (V "x")]; (Value (K 41))]
+    let astUsingLambda2 = List [Symbol "+"; (Value (K 1)); invokedLambda2]
+    let env = emptyEnv
+    match (eval invokedLambda2 env) with
+        | Value (K k) -> Assert.AreEqual (42, k)
+        | _ -> Assert.Fail()
+    Assert.Pass()
+
+[<Test>]
+let TestLambdaComplex () =
+    let parms = List [Var (V "x"); Var (V "y")]
+    let body = List [Symbol "+"; Var (V "x"); Var (V "y")]
+    let jLambda = List [List [Symbol "lambda"; parms ; body]; Value (K 1); Value(K 2)] 
+    let env = emptyEnv
+    match (eval jLambda env) with
+        | Value (K k) -> Assert.AreEqual (3, k)
         | _ -> Assert.Fail()
     Assert.Pass()
