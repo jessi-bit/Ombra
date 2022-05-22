92528c9f96f84d82bc9c13cbec46d4cd2523ea08
jessica <jessica.vecchia@gmail.com>
2022-05-22 11:36:40 +0200

    repushing

--- interpreter/ast.fs
+++ interpreter/ast.fs
@@ -70,11 +70,12 @@ let rec eval exps env =
     // printf "EVAL\n  exps: %A\n  env: %A\n" exps env
     match exps with
         | Value (K k) -> Value (K k)
-        | Var x -> Value (find x env) //try find
+        | Var x -> printf "VAR: %A and ENV : %A " x env; Value (find x env) //try find
         | Symbol s -> match Map.tryFind s symbols with
                           | Some (Function f) -> Function f
                           | _ -> failwith "symbol is not implemented"
         | List (exp::exps) ->
+            printf "exp %A\n and exps %A\n" exp exps
             match eval exp env with
                 | Function funx -> funx (List exps) env
                 | _ -> err "exp is not a function\n exp: %A\n env: %A\n" exps env
@@ -86,20 +87,21 @@ and plus exp env =
         | Value (K k) as value -> value
         | Var x ->
             printf "FINDING %A IN %A\n" x env
-            Value (find x env) //try find
-        | List (head::tail) ->
-            match head with
-                | (Value (K k)) -> let (Value (K k')) = plus (List tail) env
+            Value (find x env) 
+        | List ((head::tail) as lst) ->
+            match eval head env with
+                | (Value (K k)) -> let (Value (K k')) = plus (List tail) env //non so se funge x tre valori
                                    Value (K (k + k'))
                 // TODO array hack, fix!!!
                 | _ -> plus (eval head env) env
+
         | _ -> err "error %A %A" exp env
 
 and lambda args env = //Function (List args)
     match args with
         | List (head :: body) ->
             match head with
-                | List parms ->                     
+                | List parms ->                //let invokedLambda2 = List [List [Symbol "lambda"; List [Var (V "x")]; Var (V "x")]; (Value (K 41))]     
                     let params' = parms |> List.map (function Var (v) -> v | _ -> failwith "SONO CAZZI") //from list exp to List vars
                     Function (fun values innerEnv ->
                               match values with
@@ -109,8 +111,8 @@ and lambda args env = //Function (List args)
                                     let newEnv' = intersect env newEnv
                                     // TODO we should evaluate the whole body
                                     // TODO we have to merge both envs
-                                    printf "ENV %A\nINNERENV %A\n NEWENV %A\n" env innerEnv newEnv'
-                                    List.reduce (fun _ exp -> eval exp newEnv') body)
+                                    //printf "ENV %A\nINNERENV %A\n NEWENV %A\n" env innerEnv newEnv';
+                                    eval (List.head body) newEnv')
                 | _ -> failwith "parmeters of lambda must be a list"
         | _ -> failwith "A function must be a list"
 
@@ -123,10 +125,6 @@ and symbols =
 
 let sumAst = List [Symbol "+"; Value (K 1); Value (K 41)] 
 let res = eval sumAst (emptyEnv)
-// ((lambda (x y) (+ x y)) 1 2)
-// let parms = List [Var (V "x"); Var (V "y")]
-// let body = List [Symbol "+"; Var (V "x"); Var (V "y")]
-// let jLambda = List [List [Symbol "lambda"; parms ; body]; Value (K 1); Value(K 2)] 
 
 // TODO add a quote symbol
 let listAst = List [Value (K 1); Value (K 41)]
@@ -146,5 +144,15 @@ let res5 = eval varAst (E (Map.add (V "x") (K 42) Map.empty))
 (+ 1 ((lambda (x) x) 41))
 *)
 let invokedLambda2 = List [List [Symbol "lambda"; List [Var (V "x")]; Var (V "x")]; (Value (K 41))]
+let jTest = eval invokedLambda2 (emptyEnv)
 let astUsingLambda2 = List [Symbol "+"; (Value (K 1)); invokedLambda2]
 let res4 = eval astUsingLambda2 (emptyEnv)
+
+//((lambda (x y) (+ x y)) 1 2)
+let parms = List [Var (V "x"); Var (V "y")]
+let body = List [Symbol "+"; Var (V "x"); Var (V "y")]
+let jLambda = List [List [Symbol "lambda"; parms ; body]; Value (K 1); Value(K 2)] 
+let jTest2 = eval jLambda (emptyEnv)
+
+let jTest3 = eval (List [Symbol "+"; Var (V "x"); Var (V "y"); Value (K 5)]) (E (Map.add (V "x") (K 2) (Map.add (V "y") (K 2) Map.empty)))
+let whyreducedoesntwork = List.reduce (fun acc x -> x * 2) [1]
