43f89430e830c7e08c0ebaac19a3dc9e9474b1dd
Alberto Zaccagni <montecristoh@gmail.com>
2022-06-10 23:20:31 +0200

    cleanup

--- interpreter/ast.fs
+++ interpreter/ast.fs
@@ -24,13 +24,6 @@ type exp =
 
 type maybe<'a> = Just of 'a | Nothing
 
-// type exp2 =
-//     | Value of value
-//     | Var of var
-//     | Symbol of string
-//     | Function of (exp2 -> env -> exp2)
-// and application = exp2 list
-
 // ---------------------------------------------
 // Environment
 
@@ -43,14 +36,6 @@ let find var (E env) =
 let intersect (E outer) (E inner) =
     let res = Map.fold (fun acc k v -> Map.add k v acc) outer inner
     E res
-
-let zip vars values =
-    let rec inner vars values =
-        match (vars, values) with
-            | [], [] -> []
-            | head1 :: tail1, head2 :: tail2 -> 
-                (head1, head2) :: inner tail1 tail2
-    inner vars values |> Map.ofList
 
 // ---------------------------------------------
 // Utility
@@ -68,24 +53,13 @@ let mapInt funct exp1 exp2 =
     match exp1, exp2 with
         | Value (K k), Value (K k2) -> Value (K (funct k k2))
         | Value (K k), None -> Value (K k)
-        | _ -> failwith "not an int"
 
 let unwrapInt = function
     | Value (K k) -> k
-    | _ -> failwith "notAnint"
-
-// let neut (op: int -> int -> int) = 
-//     match op with
-//         | (+) -> Value (K (0 + 0))
-//         | (-) -> Value (K 0)
-//         | (*) -> Value (K 1)
-//         | (/) -> Value (K 1)
-
 
 // ---------------------------------------------
 // Interpreter
 //
-// TODO does adding all failWiths make this a defensive interpreter?
 
 // REMINDER:
 // If we get to this stage it means we already type checked
@@ -114,15 +88,13 @@ let rec eval exps env =
                 | Function funx -> funx (List exps) env
                 | _ -> err "exp is not a function\n exp: %A\n env: %A\n" exps env
         | _ -> failwith "wat"
-//Each operation made on ints requires an exp, an env, a function in the real world and a function in the elevated world 
-and intOp exp env funct expFun = 
+and intOp exp env funct expFun =
     match exp with
         | List [] -> None
         | Value (K _) -> exp
         | Var x -> Value (find x env)
         | List (head::tail) ->
             mapInt funct (eval head env) (expFun (List tail) env)
-        | _ -> err "error %A %A" exp env
 and plus exp env =
     intOp exp env (+) plus
 and mul exp env =
@@ -132,7 +104,6 @@ and minus exp env =
 and quote exp env =
     match exp with
         | List [lst] -> printf "%A" lst; lst
-        | _ -> failwith "exp is not a list"
 and lambda args env =
     match args with
         | List (head :: body) ->
@@ -143,16 +114,10 @@ and lambda args env =
                                     | List values ->
                                       let values' = List.map (fun x -> extractValue (eval x env)) values
                                       let params' = List.map extractVar parms
-
                                       let innerEnv = E (List.zip params' values' |> Map.ofList)
                                       let newEnv' = intersect env innerEnv
-                                      eval (List.head body) newEnv'
-                                    | _ -> failwith "Not Implemented")
-                | _ -> failwith "A lambda is a list"
-        | _ -> failwith "A function must be a list"
+                                      eval (List.head body) newEnv')
 and extractVar = function
     | Var x -> x
-    | _ -> failwith "error"
 and extractValue = function
     | Value v -> v
-    | _ -> failwith "Error"
