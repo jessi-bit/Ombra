7da9ab95aa1537e57ac888ad11b405740f2a1df1
jessica <jessica.vecchia@gmail.com>
2022-08-22 19:04:42 +0200

    new deign started

--- interpreter/interpreter.fs
+++ interpreter/interpreter.fs
@@ -1,68 +1,100 @@
 module Ombra.Interpreter
+ 
+type sexp =
+    | Float   of float
+    | String  of string
+    | Boolean of bool
+    | Symbol  of string
+    | List    of sexp list
+    | Proc    of (env -> sexp list -> sexp)
+and env =
+    | Env of Map<string, sexp>
 
-open Ombra.Types
-open Ombra.Ops
+// --------------------------------------
+// ENV
+let emptyEnv = Env Map.empty
+let extendEnv (Env env) bindings =
+    Env (List.fold (fun env' (Symbol k, v) -> Map.add k v env') env bindings)
+let lookupEnv env symbol = Map.find symbol env
 
-// ---------------------------------------------
-// Environment
+// --------------------------------------
+// INTERPRETER
 
-let print (E env) =
-    Map.iter (fun k v -> printf "key: %A - Atom: %A\n" k v) env
+let lookUp symb (Env env) =
+    match (Map.tryFind symb env) with
+        | Some sexp -> sexp
+        | _-> failwith "Not found in env"
 
-let err msg exp env =
-    failwith (sprintf msg exp env)
+let rec eval sexp env =
+    match sexp with
+        | Symbol s -> lookUp s env
+        | Float _ | String _ | Boolean _ -> sexp
+        | List (head :: tail) -> 
+            match eval head env with
+                | Proc f -> f env tail
+                | _ -> failwith "Error"
+        | _ -> failwith "ErrorEval"
 
-let find var (E env) =
-    match Map.tryFind var env with
-        | Some exp -> exp
-        | _ -> err "Var %A Not found in env %A:" var env
+let mapEval sexps env =
+    List.map (fun sexp -> eval sexp env) sexps
 
-let intersect (E outer) (E inner) =
-    let res = Map.fold (fun acc k v -> Map.add k v acc) outer inner
-    E res
+let lambda env = function
+    | [List parms; body] ->
+        Proc (fun env args ->
+            let args' = List.map (fun a -> eval a env) args
+            let bindings = List.zip parms args'
+            let env' = extendEnv env bindings
+            eval body env')
+    | _ -> failwith "Error lambda"
 
-// ---------------------------------------------
-// Lazy operations
+let aritmeticOp env operands f =
+    match (mapEval operands env) with
+        | Float f1 :: floats -> 
+            Float (List.fold (fun acc (Float f2) -> f acc f2) f1 floats)
+        | _ -> failwith "error op"
 
-let lazyOp = function
-    | "'"  -> true
-    | _ -> false
+let quote env = function 
+    | [form] -> form
+    | _ -> failwith "Error quote"
 
-//-------------------------------------------------
+let progn env args =
+    List.last (mapEval args env)
 
-// ---------------------------------------------
-// Interpreter
-//
+let operations =
+    [("+", Proc (fun env args -> aritmeticOp env args (+)) );
+     ("*", Proc (fun env args -> aritmeticOp env args (*)) );
+     ("quote", Proc (fun env form -> quote env form));
+     ("progn", Proc (fun env args -> progn env args));
+     ("lambda", Proc (lambda))
+    ] |> Map.ofList
 
-let rec evalExp e env =
-    match e with
-        | [] -> Atom Nul
-        | [Atom _] | [List _] | [Op _] -> Atom Nul
-        | head :: tail -> 
-            match (evalEl head env) with 
-                | Op s ->   let funx = Map.find s symbTable
-                            if lazyOp s then 
-                                funx tail
-                            else
-                                let evaluated = List.foldBack (fun x acc -> evalEl x env :: acc) tail []
-                                funx evaluated
-                | L (LambdaApp lambdaDef) ->
-                    let (L (LambdaDef (args, body))) = evalEl (L lambdaDef) env  
-                    let parms' = List.map (fun x -> evalEl x env) tail
-                    let innerEnv = E (List.zip args parms' |> Map.ofList)
-                    let newEnv = intersect env innerEnv 
-                    evalEl body newEnv
-                | res -> res //si può valutare solo un ITE e un LambdaDef, ma gli atomi e liste da soli produrrebbero un errore
+// ((lambda (x) (+ x 41)) 1)
+// (lambda (x) (+ x 41) 1)
+let sumLambda = List [
+            List [Symbol "lambda"; List [Symbol "x"];
+               List [Symbol "+"; Symbol "x"; Float 41];
+            ]; Float 1]
 
-and evalEl el env =
-    match el with 
-        | Atom (Var x) -> find x env
-        | Atom (_) | Op (_) | List(_) | L (_)  -> el
-        | SubExp s  -> evalExp s env
-        | ITE (cnd, thn, ls) ->  
-                                let (Atom (B condition)) = evalEl cnd env
-                                if condition then
-                                    printfn "El : %A" thn;
-                                    printfn "El : %A" (evalEl thn env);
-                                    evalEl thn env
-                                    else evalEl ls env
+let b = eval sumLambda (Env operations)
+
+let prognA = List [Symbol "progn"; Float 42]
+let p1 = eval prognA (Env operations)
+
+// | List [Symbol "let"; List bindings; body] ->
+//      let binder binding =
+//          match binding with
+//              | List [Symbol var; value] -> (var, eval value (Env env))
+//      let env' = extendEnv env (List.map binder bindings)
+//      eval body (Env env')
+// | List (Symbol funx :: args) ->
+//     let args' = List.map (fun arg -> eval arg (Env env)) args
+//     List.reduce (lookupOp funx) args'
+// | List (head :: tail) ->
+//     match eval head (Env env) with
+//         | Proc f -> f (Env env) tail      
+        
+
+// let aFun = List [Symbol "+"; Float 1.0; List [Symbol "+"; Float 2.0; Float 3.0]]
+// let b = eval aFun emptyEnv
+
+
