61dbe32cba508870dafc74a21dca8584d547f3b9
Alberto Zaccagni <montecristoh@gmail.com>
2022-11-09 18:39:15 +0100

    generating terms

--- interpreter/interpreters-verification-of-equality.fsx
+++ interpreter/interpreters-verification-of-equality.fsx
@@ -11,10 +11,8 @@ open Ombra.Interpreter.Closures
 #load "interpreter-substitution-semantics.fsx"
 open Ombra.Interpreter.Substitution
 
-//VarsSet
 let varsSet= ref<Set<ident>> Set.empty
 
-// TypeChecker -----------------------------------------------------------------
 let rec tpCheck tEnv = function
     | Lit x -> Map.tryFind x tEnv
     | Bool _ -> Some BOOL
@@ -46,39 +44,28 @@ let idGen =
     let goodVars = seq {for i in letters do if not (Set.contains i vSet) then yield i}
     Gen.elements goodVars
 
-// TODO generate a different var each time for lambda, use a global variable
-// base case: bind between the range of var
-// use lambda with types annotations
+// generation rules
+let ruleBoolean () = Gen.map Bool Arb.generate<bool>
+let ruleIdent () = Gen.map Lit idGen
+let ruleLambda generateExp size = Gen.map3 (fun i tp e -> Lam (i, tp, e))
+                                        (gen {let! id = idGen
+                                              varsSet.Value <- Set.add id varsSet.Value
+                                              return id})
+                                        (gen {let! tp = Arb.generate<ty>
+                                              return tp})
+                                        (generateExp (size / 2))
+// the first argument of an App is always a Lam
+let ruleApp generateExp size = Gen.map2 (fun e e' -> App (e, e')) (ruleLambda generateExp (size / 2)) (generateExp (size / 2))
+let ruleIfe generateExp size = Gen.map3 (fun cond e' e'' -> If (cond, e', e'')) (generateExp (size / 2)) (generateExp (size / 2)) (generateExp (size / 2))
+
 let rec generateExp size =
     match size with
-        | 0 -> Gen.oneof [
-            Gen.map Lit (Arb.generate<ident> |> Gen.filter goodId)
-            Gen.map Bool Arb.generate<bool>]
+        | 0 -> Gen.oneof [ruleBoolean (); ruleIdent ()]
         | n when n > 0 ->
-            Gen.frequency [  
-                (1, Gen.map Lit (Arb.generate<ident> |> Gen.filter goodId)); 
-                (1, Gen.map Bool Arb.generate<bool>);
-                (4, Gen.map3 (fun i tp e -> Lam (i, tp, e)) (Gen.map id (Arb.generate<ident> |> Gen.filter goodId)) (Gen.map id (Arb.generate<ty>)) (generateExp (size / 2)));
-                (5, Gen.map2 (fun e e' -> App (e, e')) (generateExp (size / 2)) (generateExp (size / 2)))
-            ]
-
-let rec generateExp2 size =
-    match size with
-        | 0 -> Gen.oneof [
-            Gen.map Lit idGen
-            Gen.map Bool Arb.generate<bool>]
-        | n when n > 0 ->
-            Gen.frequency [  
-                (1, Gen.map Lit idGen); 
-                (1, Gen.map Bool Arb.generate<bool>);
-                (3, Gen.map3 (fun i tp e -> Lam (i, tp, e)) (gen {let! id = idGen
-                                                                  varsSet.Value<-Set.add id varsSet.Value
-                                                                  return id}) 
-                                                            (gen {let! tp = Arb.generate<ty>
-                                                                  return tp}) 
-                                                            (generateExp2 (size / 2)));
-                (6, Gen.map2 (fun e e' -> App (e, e')) (generateExp2 (size / 2)) (generateExp2 (size / 2)));
-                (6, Gen.map3 (fun cond e' e'' -> If (cond, e', e'')) (generateExp2 (size / 2)) (generateExp2 (size / 2)) (generateExp2 (size / 2)))
+            Gen.oneof [
+                ruleLambda generateExp (size / 2)
+                ruleApp generateExp (size / 2)
+                ruleIfe generateExp (size / 2)
             ]
 
 let rec verifyequality valueC valueS envC =
@@ -93,14 +80,7 @@ let rec verifyequality valueC valueS envC =
                     id1 = id2 && (verifyequality e1' e2' envC)
 
 let propVal = 
-            Gen.sized generateExp2 
-            //seq {for e in (Gen.sized generateExp2) do if tpCheck Map.empty e <> None then yield e}
-            //Gen.sized generateExp2
-            // |> Gen.filter (fun exp ->
-            //                match (tpCheck Map.empty exp) with
-            //                    | Some _ -> true
-            //                    | _ -> printf "\nFAILED\n%A\n\n" exp
-            //                           false)
+            Gen.sized generateExp
             |> Gen.filter (fun exp -> tpCheck Map.empty exp <> None)
             |> Arb.fromGen
             |> Prop.forAll <| fun (ast) ->
